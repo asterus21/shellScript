0543c69b374929556a15362a443f23b4803c0dfb
Mikhail Palvov <mpavlov@megaputer.ru>
2025-07-23 09:48:28 +0300

    The Script logic given in a separate function

--- compileHelp.sh
+++ compileHelp.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 shopt -s extglob
 
-# TODO: ./helpCompile --path or -p changes the path_to_license_file value (only absolute paths are allowed, otherwise a warning occures) (mind the use of slashes)
-# TODO: ./helpCompile --data or -d changes both of the path_to_build_windows and path_to_build_for_command_line values (only absolute paths are allowed, otherwise a warning occures) (mind the use of slashes) (see the slashes change)
-# TODO: ./helpCompile --script or -s changes the path_to_main_script value (only absolute paths are allowed, otherwise a warning occures) (mind the use of slashes)
+# TODO: ./helpCompile --path or -p changes the default_path_to_license_file value (only absolute paths are allowed, otherwise a warning occures)
+# TODO: ./helpCompile --data or -d changes the default_path_to_build value (only absolute paths are allowed, otherwise a warning occures)
+# TODO: ./helpCompile --script or -s changes the default_path_to_main_script value (only absolute paths are allowed, otherwise a warning occures)
 
 ######### the list of the constants #########
 
 # path to the license file
-declare path_to_license_file=D:\\pa_config\\comlicbits.h
+declare default_path_to_license_file=D:\\pa_config\\comlicbits.h
 
 # path to the Build folder for the script
-declare path_to_build_windows=C:\\pa6autotests\\Builds
-
-# path to the Build folder for the Comand Line Interface
-declare path_to_build_for_command_line=C:\\pa6autotests\\Builds
+declare default_path_to_build=C:\\pa6autotests\\Builds
 
 # path to the Main script file
-# declare path_to_main_script=D:\\gitbash\\help\\testBuild.cmd
-declare path_to_main_script=D:\\gitbash\\help\\testBuild.cmd
+declare default_path_to_main_script=D:\\gitbash\\help\\testBuild.cmd
 
 ######### the functions in use #########
 
+# timestamp print function
 print() {
   printf '%(%Y/%m/%d %H:%M:%S)T '
   echo "$1"
 }
 
+# timestamp print function to exit the program
 print_exit() {
   print "Exiting..." && print "Exited"
 }
 
+# function to ask a user whether they want to start the Main script
 ask_sure() {
   print "Are you sure you want to continue (Y/N)?"
   while read -r -n 1 -s answer; do
@@ -44,6 +43,7 @@ ask_sure() {
   return "$retval"
 }
 
+# function to show a list of arguments
 show_arguments() {
   print "Here is a list of arguments:"
   print "Add --help or -h to show this list of arguments."
@@ -54,28 +54,16 @@ show_arguments() {
   print "Add --script or -s to change the path to the main script (only absolute paths are allowed)."
 }
 
+# function to show a list of builds
 show_builds() {
-  local windows_path
-  windows_path="${path_to_build_windows%\\}"
-
-  for entry in "$path_to_build_for_command_line"/*; do
+  for entry in "$default_path_to_build"/*; do
     local build_name
     build_name=$(basename "$entry")
-    print "${windows_path}\\${build_name}"
+    print "${default_path_to_build}\\${build_name}"
   done
 }
 
-##### the script is given in a separate function #####
-
-start_script_with_build_number() {
-  build_path="${path_to_build_windows}\\$1"
-  check_paths "$build_path"
-}
-
-start_main_script() {
-  $path_to_main_script
-}
-
+# function to check paths
 check_paths() {
   build_path="$1"
   print "The build folder path: $build_path"
@@ -98,24 +86,18 @@ check_paths() {
     print "The script is about to start..."
     ask_sure
     echo ""
-  fi  
+  fi
 
   # path to the nodejs to compile the User Manual with the API specification
   node_js_path="$build_path\\Bin64\\nodejs"
-
-  # PDF files are not copied, that's why we need to cut them before the script starts and insert afterwards
-  mv "$build_path\\SourceData\\www\\help\\pdf" "$build_path\\SourceData\\www"
-
-  export SOURCEDATA=$build_path\\SourceData
-  export COMLICBITSPATH=${path_to_license_file}
-  export MISHARED=${node_js_path}
-
-  start_main_script
-
-  mv "$build_path\\SourceData\\www\\pdf" "$build_path\\SourceData\\www\\help"
+  if [ ! -d "$node_js_path" ]; then
+      print "The NodeJS folder is not found."
+      print_exit
+      exit 1
+  fi
 }
 
-start_script() {
+start_script_without_build_number() {
   print "Starting the script..."
   print "Getting a list of folders..."
   print "Note that there can be several builds in the folder."
@@ -154,15 +136,40 @@ start_script() {
       exit 0 ;;
   esac
 
-  build_path="${path_to_build_windows}\\${build_number}"
+  build_path="${default_path_to_build}\\${build_number}"
   check_paths "$build_path"
+  start_main_script
 }
 
-######### the input check #########
+start_script_with_build_number() {
+  build_path="${default_path_to_build}\\$1"
+  check_paths "$build_path"
+  start_main_script
+}
+
+##### the main script #####
+
+start_main_script() {
+  # PDF files are not copied, that's why we need to cut them before the script starts and insert afterwards
+  mv "$build_path\\SourceData\\www\\help\\pdf" "$build_path\\SourceData\\www"
+
+  # export of the environmental variables
+  export SOURCEDATA=$build_path\\SourceData
+  export COMLICBITSPATH=${default_path_to_license_file}
+  export MISHARED=${node_js_path}
+
+  # the main script call
+  $default_path_to_main_script
+
+  # PDF files are not copied, that's why we need to cut them before the script starts and insert afterwards
+  mv "$build_path\\SourceData\\www\\pdf" "$build_path\\SourceData\\www\\help"
+}
+
+######### the scripts calls #########
 
 case "$1" in
     "" | " " )
-      start_script ;;
+      start_script_without_build_number ;;
     "-l" | "--list" )
       print "Here is a list of builds:"
       show_builds
